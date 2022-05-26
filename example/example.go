b33409d642abd9a6a6ac7dcafef1bf67bebe6c90
Your Name <you@example.com>
2022-05-27 00:30:14 +0800

    demo加入推送数据

--- example/example.go
+++ example/example.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"strings"
 	"time"
+
+	"example/wss"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -12,24 +15,86 @@ import (
 
 var askQueue *trading_engine.OrderQueue
 var bidQueue *trading_engine.OrderQueue
+var sendMsg chan []byte
+var web *gin.Engine
 
 func main() {
-	g := gin.New()
+
 	gin.SetMode(gin.DebugMode)
-	g.LoadHTMLGlob("./*.html")
+
+	startWeb()
+}
+
+func startWeb() {
+	web = gin.New()
+	web.LoadHTMLGlob("./*.html")
 	askQueue = trading_engine.NewQueue()
 	bidQueue = trading_engine.NewQueue()
+	sendMsg = make(chan []byte, 100)
 
-	g.GET("/depth", func(c *gin.Context) {
-		a := askQueue.GetDepth()
-		b := bidQueue.GetDepth()
+	go pushDepth()
 
-		c.JSON(200, gin.H{
-			"ask": a,
-			"bid": b,
-		})
+	web.GET("/api/depth", depth)
+	web.POST("/api/new_order", newOrder)
+
+	web.GET("/demo", func(c *gin.Context) {
+		c.HTML(200, "demo.html", nil)
 	})
 
+	//websocket
+	{
+		wss.HHub = wss.NewHub()
+		go wss.HHub.Run()
+		go func() {
+			for {
+				select {
+				case data := <-sendMsg:
+					wss.HHub.Send(data)
+				default:
+					time.Sleep(time.Duration(100) * time.Millisecond)
+				}
+			}
+		}()
+
+		web.GET("/ws", wss.ServeWs)
+		web.GET("/pong", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+	web.Run(":8080")
+}
+
+func depth(c *gin.Context) {
+	a := askQueue.GetDepth()
+	b := bidQueue.GetDepth()
+
+	c.JSON(200, gin.H{
+		"ask": a,
+		"bid": b,
+	})
+}
+
+func pushDepth() {
+	for {
+		ask := askQueue.GetDepth()
+		bid := bidQueue.GetDepth()
+		data := gin.H{
+			"tag": "depth",
+			"data": gin.H{
+				"ask": ask,
+				"bid": bid,
+			},
+		}
+		msg, _ := json.Marshal(data)
+		sendMsg <- []byte(msg)
+		time.Sleep(time.Duration(500) * time.Millisecond)
+
+	}
+}
+
+func newOrder(c *gin.Context) {
 	type args struct {
 		OrderId   string `json:"order_id"`
 		OrderType string `json:"order_type"`
@@ -37,42 +102,32 @@ func main() {
 		Quantity  string `json:"quantity"`
 	}
 
-	g.POST("/new_order", func(c *gin.Context) {
-		var param args
-		c.BindJSON(&param)
+	var param args
+	c.BindJSON(&param)
 
-		if param.Price == "" || param.Quantity == "" {
-			c.Abort()
-			return
-		}
+	if param.Price == "" || param.Quantity == "" {
+		c.Abort()
+		return
+	}
 
-		// rand.Seed(time.Now().Unix())
-		// rand_price := rand.Float64()
+	// rand.Seed(time.Now().Unix())
+	// rand_price := rand.Float64()
 
-		if strings.ToLower(param.OrderType) == "ask" {
-			askOrder := trading_engine.NewAskItem(uuid.NewString(), string2decimal(param.Price), string2decimal(param.Quantity), time.Now().Unix())
-			askQueue.Push(askOrder)
-		} else {
-			bidOrder := trading_engine.NewBidItem(uuid.NewString(), string2decimal(param.Price), string2decimal(param.Quantity), time.Now().Unix())
-			bidQueue.Push(bidOrder)
-		}
+	if strings.ToLower(param.OrderType) == "ask" {
+		askOrder := trading_engine.NewAskItem(uuid.NewString(), string2decimal(param.Price), string2decimal(param.Quantity), time.Now().Unix())
+		askQueue.Push(askOrder)
+	} else {
+		bidOrder := trading_engine.NewBidItem(uuid.NewString(), string2decimal(param.Price), string2decimal(param.Quantity), time.Now().Unix())
+		bidQueue.Push(bidOrder)
+	}
 
-		c.JSON(200, gin.H{
-			"ok": true,
-			"data": gin.H{
-				"ask_len": askQueue.Len(),
-				"bid_len": bidQueue.Len(),
-			},
-		})
+	c.JSON(200, gin.H{
+		"ok": true,
+		"data": gin.H{
+			"ask_len": askQueue.Len(),
+			"bid_len": bidQueue.Len(),
+		},
 	})
-
-	g.GET("/demo", func(c *gin.Context) {
-		c.HTML(200, "demo.html", nil)
-	})
-
-	//websocket
-
-	g.Run(":8080")
 }
 
 func string2decimal(a string) decimal.Decimal {
