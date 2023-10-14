542ccbf74954910dcd14cc26bd994884f705a6c7
Your Name <you@example.com>
2023-10-14 11:12:39 +0800

    update

--- cmd/haobase/www/middle/base.go
+++ cmd/haobase/www/middle/base.go
@@ -2,16 +2,23 @@ package middle
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/yzimhao/trading_engine/utils"
 	"github.com/yzimhao/trading_engine/utils/app"
 )
 
 func CheckLogin() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Set("user_id", "")
+		user_id := ""
 		if app.RunMode == app.ModeDemo {
-			user_id := c.GetHeader("user_id")
-			c.Set("user_id", user_id)
+			user_id = c.GetHeader("UserId")
 		}
+
+		if user_id == "" {
+			utils.ResponseFailJson(c, "需要登录")
+			return
+		}
+
+		c.Set("user_id", user_id)
 
 		// before request
 		c.Next()
