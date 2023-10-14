7791c17ac4002737254e70d2772e7f7dbe3e8521
Your Name <you@example.com>
2023-10-14 11:23:59 +0800

    update

--- cmd/haobase/www/order.go
+++ cmd/haobase/www/order.go
@@ -9,14 +9,12 @@ import (
 )
 
 type order_create_request_args struct {
-	User string `json:"user"` //test时使用
-
-	Symbol    string `json:"symbol" binding:"required" example:"eurusd"`
-	Side      string `json:"side" binding:"required" example:"sell/buy"`
-	OrderType string `json:"order_type" binding:"required" example:"limit/market"`
-	Price     string `json:"price" example:"1.00"`
-	Quantity  string `json:"qty" example:"12"`
-	Amount    string `json:"amount" example:"100.00"`
+	Symbol    string                 `json:"symbol" binding:"required"`
+	Side      trading_core.OrderSide `json:"side" binding:"required"`
+	OrderType trading_core.OrderType `json:"order_type" binding:"required"`
+	Price     string                 `json:"price" example:"1.00"`
+	Quantity  string                 `json:"qty" example:"12"`
+	Amount    string                 `json:"amount" example:"100.00"`
 }
 
 func order_create(ctx *gin.Context) {
@@ -29,15 +27,15 @@ func order_create(ctx *gin.Context) {
 	var info *orders.Order
 	var err error
 
-	side := trading_core.OrderSide(req.Side)
+	user_id := ctx.MustGet("user_id").(string)
 
-	if req.OrderType == trading_core.OrderTypeLimit.String() {
-		info, err = orders.NewLimitOrder(req.User, req.Symbol, side, req.Price, req.Quantity)
-	} else if req.OrderType == trading_core.OrderTypeMarket.String() {
+	if req.OrderType == trading_core.OrderTypeLimit {
+		info, err = orders.NewLimitOrder(user_id, req.Symbol, req.Side, req.Price, req.Quantity)
+	} else if req.OrderType == trading_core.OrderTypeMarket {
 		if utils.D(req.Amount).Cmp(decimal.Zero) > 0 {
-			info, err = orders.NewMarketOrderByAmount(req.User, req.Symbol, side, req.Amount)
+			info, err = orders.NewMarketOrderByAmount(user_id, req.Symbol, req.Side, req.Amount)
 		} else if utils.D(req.Quantity).Cmp(decimal.Zero) > 0 {
-			info, err = orders.NewMarketOrderByQty(req.User, req.Symbol, side, req.Quantity)
+			info, err = orders.NewMarketOrderByQty(user_id, req.Symbol, req.Side, req.Quantity)
 		}
 	}
 	if err != nil {
