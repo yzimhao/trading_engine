fede9ee5ff1ba7babe00880dbeb2d1d5c5b1a206
Your Name <you@example.com>
2023-10-19 10:45:24 +0800

    1

--- cmd/haotrader/main.go
+++ cmd/haotrader/main.go
@@ -22,7 +22,10 @@ func main() {
 			&cli.BoolFlag{Name: "deamon", Value: false, Aliases: []string{"d"}},
 		},
 
-		Before: func(cCtx *cli.Context) error {
+		Before: func(ctx *cli.Context) error {
+			app.ConfigInit(ctx.String("config"))
+			app.DatabaseInit(app.Cstring("database.driver"), app.Cstring("database.dsn"), app.Cbool("database.show_sql"))
+			app.RedisInit(app.Cstring("redis.host"), app.Cstring("redis.password"), app.Cint("redis.db"))
 			return nil
 		},
 
@@ -60,14 +63,10 @@ func main() {
 			},
 		},
 		Action: func(ctx *cli.Context) error {
-			app.ConfigInit(ctx.String("config"))
 			app.LogsInit("haotrader.run", ctx.Bool("deamon"))
-
 			if viper.GetString("main.mode") != "prod" {
 				logrus.Infof("当前运行在%s模式下，生产环境时main.mode请务必成prod", viper.GetString("main.mode"))
 			}
-
-			app.RedisInit(app.Cstring("redis.host"), app.Cstring("redis.password"), app.Cint("redis.db"))
 
 			if ctx.Bool("deamon") {
 				logrus.Info("开始守护进程")
