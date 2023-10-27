5171f55aa23cd5f1a072d8a12c3ef2fd63e00347
Your Name <you@example.com>
2023-10-27 23:00:54 +0800

    login界面

--- cmd/haoadm/view/template_func.go
+++ cmd/haoadm/view/template_func.go
@@ -3,6 +3,7 @@ package view
 import (
 	"fmt"
 	"html/template"
+	"time"
 
 	"github.com/yzimhao/trading_engine/utils/app"
 	"github.com/yzimhao/trading_engine/utils/app/config"
@@ -11,13 +12,25 @@ import (
 func templateFuncMap() template.FuncMap {
 	return template.FuncMap{
 		"app_power_by": func() string {
-			return fmt.Sprintf("powered by Haotrader %s", app.Version)
+			return fmt.Sprintf("powered by <a target='_blank' href='https://github.com/yzimhao/trading_engine'>HaoTrader %s</a>", app.Version)
 		},
 		"app_short_name": func() string {
 			return "HT"
 		},
 		"version": func() string {
 			return app.Version
+		},
+		"author": func() string {
+			return "yzimhao"
+		},
+		"author_link": func() string {
+			return "https://github.com/yzimhao"
+		},
+		"year": func() string {
+			return time.Now().Format("2006")
+		},
+		"unsafe": func(s string) template.HTML {
+			return template.HTML(s)
 		},
 		"site_name": func() string {
 			return config.App.Haoadm.SiteName
