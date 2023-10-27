2ff38be679ed9bdc8d12589dd0601a96a6523711
Your Name <you@example.com>
2023-10-27 21:44:44 +0800

    版权和版本信息

--- cmd/haoadm/view/template_func.go
+++ cmd/haoadm/view/template_func.go
@@ -1,17 +1,24 @@
 package view
 
 import (
+	"fmt"
 	"html/template"
 
+	"github.com/yzimhao/trading_engine/utils/app"
 	"github.com/yzimhao/trading_engine/utils/app/config"
 )
 
 func templateFuncMap() template.FuncMap {
 	return template.FuncMap{
-		"app_name": func() string {
-			return "HaoTrader"
+		"app_power_by": func() string {
+			return fmt.Sprintf("powered by Haotrader %s", app.Version)
 		},
-
+		"app_short_name": func() string {
+			return "HT"
+		},
+		"version": func() string {
+			return app.Version
+		},
 		"site_name": func() string {
 			return config.App.Haoadm.SiteName
 		},
