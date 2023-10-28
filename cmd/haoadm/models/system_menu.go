c4f87906d64cd3c1f633023029a7db7ff457ea0f
Your Name <you@example.com>
2023-10-28 14:28:18 +0800

    icon

--- cmd/haoadm/models/system_menu.go
+++ cmd/haoadm/models/system_menu.go
@@ -23,7 +23,7 @@ var (
 func init() {
 	menuList = []SystemMenu{
 		// SystemMenu{Id: 1, Pid: 0, Title: "系统", Icon: "fa fa-wrench", Href: "javascript:;", Target: "_self"},
-		SystemMenu{Id: 2, Pid: 0, Title: "交易", Icon: "fa fa-usd", Href: "javascript:;", Target: "_self"},
+		SystemMenu{Id: 2, Pid: 0, Title: "交易", Icon: "fa fa-btc", Href: "javascript:;", Target: "_self"},
 		SystemMenu{Id: 3, Pid: 0, Title: "用户", Icon: "fa fa-user", Href: "javascript:;", Target: "_self"},
 		SystemMenu{Id: 4, Pid: 0, Title: "统计", Icon: "fa fa-bar-chart", Href: "javascript:;", Target: "_self"},
 
