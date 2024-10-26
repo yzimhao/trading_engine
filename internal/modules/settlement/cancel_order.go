d60304bbf692511ff96c6bbe8730cc6178476587
Your Name <you@example.com>
2024-10-26 16:13:35 +0800

    update

---
+++ internal/modules/settlement/cancel_order.go
@@ -0,0 +1,39 @@
+package settlement
+
+import (
+	"context"
+
+	"github.com/duolacloud/broker-core"
+	"github.com/yzimhao/trading_engine/v2/internal/models/types"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+)
+
+type inCancelOrderContext struct {
+	fx.In
+	Broker broker.Broker
+	Logger *zap.Logger
+}
+
+type CancelOrderSubscriber struct {
+	broker broker.Broker
+	logger *zap.Logger
+}
+
+func NewCancelOrderSubscriber(in inCancelOrderContext) *CancelOrderSubscriber {
+	return &CancelOrderSubscriber{
+		broker: in.Broker,
+		logger: in.Logger,
+	}
+}
+
+func (s *CancelOrderSubscriber) Subscribe() {
+	s.broker.Subscribe(types.TOPIC_ORDER_CANCEL, s.On)
+}
+
+func (s *CancelOrderSubscriber) On(ctx context.Context, event broker.Event) error {
+	s.logger.Info("cancel order", zap.Any("event", event))
+	//TODO
+
+	return nil
+}
--- internal/modules/settlement/module.go
+++ internal/modules/settlement/module.go
@@ -7,10 +7,12 @@ import (
 var Module = fx.Options(
 	fx.Provide(
 		NewSettlementSubscriber,
+		NewCancelOrderSubscriber,
 	),
 	fx.Invoke(startupSubscriber),
 )
 
-func startupSubscriber(subscriber *SettlementSubscriber) {
+func startupSubscriber(subscriber *SettlementSubscriber, cancelOrder *CancelOrderSubscriber) {
 	subscriber.Subscribe()
+	cancelOrder.Subscribe()
 }
