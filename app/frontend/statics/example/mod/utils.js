2cdfc8afc1c21a59c0f8dfc2b1bf302427e20126
Your Name <you@example.com>
2024-12-29 21:04:58 +0800

    fix time formart

--- app/frontend/statics/example/mod/utils.js
+++ app/frontend/statics/example/mod/utils.js
@@ -6,7 +6,13 @@ layui.define(['laytpl'], function(exports){
     var obj = {
         formatTime: function(t) {
             var d = new Date(parseInt(t));
-            return d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+            var month = (d.getMonth()+1) <10 ? "0"+d.getMonth()+1 : d.getMonth() +1;
+            var date = d.getDate() <10 ? "0"+d.getDate() : d.getDate();
+            var hours = d.getHours() <10 ? "0"+d.getHours() : d.getHours();
+            var minutes = d.getMinutes() <10 ? "0"+d.getMinutes() : d.getMinutes();
+            var seconds = d.getSeconds() <10 ? "0"+d.getSeconds() : d.getSeconds();
+
+            return d.getFullYear() + '-' + month + '-' + date + ' ' + hours + ':' + minutes + ':' + seconds;
         },
 
         rendertradelog: function(data) {
