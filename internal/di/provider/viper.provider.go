070eea0f5c249a7298d6ec51bdb7f9d4c108cc36
Your Name <you@example.com>
2025-06-05 16:36:20 +0800

    add isdevelopment

--- internal/di/provider/viper.provider.go
+++ internal/di/provider/viper.provider.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"os"
 	"path"
 	"runtime"
 	"strings"
@@ -15,6 +16,13 @@ func NewViper(logger *zap.Logger) *viper.Viper {
 	)
 
 	v.AutomaticEnv()
+	// for unit test
+	if IsDevelopment() {
+		_, current, _, _ := runtime.Caller(0)
+		root := path.Dir(path.Dir(path.Dir(path.Dir(current))))
+		logger.Sugar().Infof("root: %s", root)
+		v.AddConfigPath(root)
+	}
 
 	logger.Sugar().Infof("config file: %s", v.ConfigFileUsed())
 	return v
@@ -24,4 +32,15 @@ func Root() string {
 	_, current, _, _ := runtime.Caller(0)
 	root := path.Dir(path.Dir(path.Dir(path.Dir(current))))
 	return root
+}
+
+func IsDevelopment() bool {
+	exe, err := os.Executable()
+	if err != nil {
+		return false
+	}
+	if strings.Contains(exe, "__debug") {
+		return true
+	}
+	return strings.Contains(exe, os.TempDir())
 }
