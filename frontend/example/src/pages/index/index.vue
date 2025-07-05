d73879e6a3d0b6cff7b354210f09173a5761739c
Your Name <you@example.com>
2025-07-05 23:12:38 +0800

    example

--- frontend/example/src/pages/index/index.vue
+++ frontend/example/src/pages/index/index.vue
@@ -1,14 +1,97 @@
 <template>
   <view class="content">
     <view class="left">
-        <view class="kline-chart"></view>
-        <view class="user-form mtop10"></view>
+        <view class="kline-chart">
+            <text>chart loading...</text>
+        </view>
+        <view class="user-form mtop10">
+            <view class="user-assets">
+                <view  class="notlogin">
+                    <view>
+                        <text>游客，请登陆</text>
+                    </view>
+                    <view class="line1">
+                        <text>用户名</text>
+                        <uni-easyinput type="text" placeholder="随便输入一串字符串" style="width: 200px;" />
+                    </view>
+                    <view class="line1">
+                        <button type="primary" size="mini">登陆</button>
+                    </view>
+                </view>
+                <view v-if="false" class="line1">
+                    <text>Hi, 132</text>
+                    <text> [退出] </text>
+                </view>
+            </view>
+            <view class="user-area">
+                <view class="sell">
+                    <view class="line1">
+                        <text class="item-title">类型</text>
+                        <uni-data-checkbox v-model="range.sellOrderTypeVal" :localdata='range.orderType'></uni-data-checkbox>
+                    </view>
+                    <view class="line1">
+                        <text class="item-title">价格</text>
+                        <uni-easyinput type="digit" placeholder="1.00" style="width: 200px;" />
+                    </view>
+                    <view class="line1">
+                        <text class="item-title">数量</text>
+                        <uni-easyinput type="digit" placeholder="10" style="width: 200px;"  />
+                    </view>
+                    <view class="line1">
+                        <button type="primary" size="mini">卖出</button>
+                        <text>可用BTC: 1000.00</text>
+                    </view>
+                </view>
+                <view class="buy">
+                    <view class="line1">
+                        <text class="item-title">类型</text>
+                        <uni-data-checkbox v-model="range.buyOrderTypeVal" :localdata='range.orderType'></uni-data-checkbox>
+                    </view>
+                    <view class="line1">
+                        <text class="item-title">价格</text>
+                        <uni-easyinput type="digit" placeholder="1.00" />
+                    </view>
+                    <view class="line1">
+                        <text class="item-title">数量</text>
+                        <uni-easyinput type="digit" placeholder="10" />
+                    </view>
+                    <view class="line1">
+                        <button type="primary" size="mini">买入</button>
+                        <text>可用USDT: 98.00</text>
+                    </view>
+                </view>
+            </view>
+        </view>
     </view>
     <view class="right">
         <view class="orderbook">
             <text class="orderbook-title">orderbook</text>
             
             <view class="ask">
+                <uni-row>
+                    <uni-col :span="12">1.03</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">1.04</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">1.01</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">1.02</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">1.03</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">1.04</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
                 <uni-row>
                     <uni-col :span="12">1.01</uni-col>
                     <uni-col :span="12">1000</uni-col>
@@ -34,7 +117,39 @@
 
             <view class="bid">
                 <uni-row>
-                    <uni-col :span="12">1.00</uni-col>
+                    <uni-col :span="12">0.99</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">0.99</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">0.99</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">0.99</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">0.99</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">0.99</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">0.99</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">0.99</uni-col>
+                    <uni-col :span="12">1000</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">0.99</uni-col>
                     <uni-col :span="12">1000</uni-col>
                 </uni-row>
                 <uni-row>
@@ -44,7 +159,75 @@
                 
             </view>
         </view>
-        <view class="tradehistory mtop10"></view>
+        <view class="tradehistory mtop10">
+            <view>
+                <uni-row>
+                    <uni-col :span="12">成交时间</uni-col>
+                    <uni-col :span="6">成交价格</uni-col>
+                    <uni-col :span="6">成交量</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">2025-07-05 20:11:56.789</uni-col>
+                    <uni-col :span="6">10.00</uni-col>
+                    <uni-col :span="6">100</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">2025-07-05 20:11:56.789</uni-col>
+                    <uni-col :span="6">10.00</uni-col>
+                    <uni-col :span="6">100</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">2025-07-05 20:11:56.789</uni-col>
+                    <uni-col :span="6">10.00</uni-col>
+                    <uni-col :span="6">100</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">2025-07-05 20:11:56.789</uni-col>
+                    <uni-col :span="6">10.00</uni-col>
+                    <uni-col :span="6">100</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">2025-07-05 20:11:56.789</uni-col>
+                    <uni-col :span="6">10.00</uni-col>
+                    <uni-col :span="6">100</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">2025-07-05 20:11:56.789</uni-col>
+                    <uni-col :span="6">10.00</uni-col>
+                    <uni-col :span="6">100</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">2025-07-05 20:11:56.789</uni-col>
+                    <uni-col :span="6">10.00</uni-col>
+                    <uni-col :span="6">100</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">2025-07-05 20:11:56.789</uni-col>
+                    <uni-col :span="6">10.00</uni-col>
+                    <uni-col :span="6">100</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">2025-07-05 20:11:56.789</uni-col>
+                    <uni-col :span="6">10.00</uni-col>
+                    <uni-col :span="6">100</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">2025-07-05 20:11:56.789</uni-col>
+                    <uni-col :span="6">10.00</uni-col>
+                    <uni-col :span="6">100</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">2025-07-05 20:11:56.789</uni-col>
+                    <uni-col :span="6">10.00</uni-col>
+                    <uni-col :span="6">100</uni-col>
+                </uni-row>
+                <uni-row>
+                    <uni-col :span="12">2025-07-05 20:11:56.789</uni-col>
+                    <uni-col :span="6">10.00</uni-col>
+                    <uni-col :span="6">100</uni-col>
+                </uni-row>
+            </view>
+        </view>
     </view>
 
     
@@ -55,7 +238,14 @@
 export default {
   data() {
     return {
-      title: 'Home',
+        range: {
+            orderType: [{"value": 0,"text": "限价"	},{"value": 1,"text": "市价"}],
+            sellOrderTypeVal: 0,
+            buyOrderTypeVal: 0
+        },
+        data: {
+            
+        }
     }
   },
   onLoad() {},
