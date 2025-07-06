141905abdd793fdf319d553a34be732aadfef9df
Your Name <you@example.com>
2025-07-06 21:47:04 +0800

    utils

--- frontend/example/src/example/index.vue
+++ frontend/example/src/example/index.vue
@@ -149,10 +149,9 @@
 </template>
 
 <script>
-import {
-    request
-} from '@/common/request.js'
+import { request } from '@/common/request.js';
 import { socketInit } from '@/common/websocket.js'; 
+import { formatTimestamp } from '@/common/utils.js';
 
 export default {
   data() {
@@ -309,20 +308,7 @@ export default {
             }
         };
     },
-    formatTimestamp(value){
-        const timestamp = parseInt(value) / 1e6;
-        const date = new Date(timestamp);
-        
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        const seconds = String(date.getSeconds()).padStart(2, '0');
-        const milliseconds = String(parseInt(value.slice(-6, -3))).padStart(3, '0');
-        
-        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
-    },
+    
     actionLogin () {
         const me = this;
         console.log(me.user);
